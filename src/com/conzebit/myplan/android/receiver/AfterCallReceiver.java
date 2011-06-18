c91dc2262b0d5b05321f3548382f0e43fe050bc6
José Miguel Sanz <jmsanzg@gmail.com>
2011-06-18 20:05:03 +0200

    Issue #2 - Al finalizar llamada el importe no siempre es correcto

--- src/com/conzebit/myplan/android/receiver/AfterCallReceiver.java
+++ src/com/conzebit/myplan/android/receiver/AfterCallReceiver.java
@@ -16,49 +16,89 @@
  */
 package com.conzebit.myplan.android.receiver;
 
+import java.util.ArrayList;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.TelephonyManager;
 
-import com.conzebit.myplan.android.activity.AfterCallActivity;
+import com.conzebit.myplan.R;
+import com.conzebit.myplan.android.activity.MainActivity;
 import com.conzebit.myplan.android.store.AndroidMsisdnTypeStore;
 import com.conzebit.myplan.android.store.LogStoreService;
 import com.conzebit.myplan.android.util.Settings;
+import com.conzebit.myplan.core.Chargeable;
 import com.conzebit.myplan.core.call.Call;
 import com.conzebit.myplan.core.msisdn.MsisdnTypeService;
+import com.conzebit.myplan.core.plan.PlanChargeable;
+import com.conzebit.myplan.core.plan.PlanService;
+import com.conzebit.myplan.core.plan.PlanSummary;
+import com.conzebit.util.Formatter;
 
 public class AfterCallReceiver extends BroadcastReceiver {
 
 	private static boolean incomingCall = false;
+	
+	private static final int NOTIFICATION_ID = 1;
+	private static NotificationManager notificationManager = null;
 	
 	@Override
     public void onReceive(Context context, Intent intent) {
     	if (!Settings.isTrapAfterCall(context)) {
     		return;
     	}
-		
-    	TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
     	
-    	switch(tm.getCallState()) {
-    		case TelephonyManager.CALL_STATE_OFFHOOK:
-    			break;
-    		case TelephonyManager.CALL_STATE_IDLE:
+    	if (notificationManager == null) {
+    		notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);;
+    	}
+
+    	Bundle extras = intent.getExtras();
+		if (extras != null) {
+			String state = extras.getString(TelephonyManager.EXTRA_STATE);
+			if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
+				incomingCall = true;
+			} else if (TelephonyManager.EXTRA_STATE_IDLE.equals(state)) {
             	if (!incomingCall) {
             		AndroidMsisdnTypeStore androidMsisdnTypeStore = new AndroidMsisdnTypeStore(context);
             		MsisdnTypeService.getInstance(androidMsisdnTypeStore);
             		Call lastCall = LogStoreService.getInstance().getLastCall(context);
             		if (lastCall != null && lastCall.getDuration() > 0 && lastCall.getType() == Call.CALL_TYPE_SENT) {
-	            		Intent i = new Intent(context, AfterCallActivity.class);
-	            		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 
-	            		context.startActivity(i);
+            			notifyUser(context);
             		}
             	}
             	incomingCall = false;
-            	break;
-    		case TelephonyManager.CALL_STATE_RINGING:
-            	incomingCall = true;
-            	break;
-    	}
+			}
+		}    	
     }
+	
+	private void notifyUser(Context context) {
+		ArrayList<Chargeable> data = LogStoreService.getInstance().get(context, Settings.getBillingStartDate(context), Settings.getBillingEndDate(context));
+		String operator = Settings.getOperator(context);
+		String planName = Settings.getMyPlan(context);
+		PlanService planService = PlanService.getInstance();
+		PlanSummary summary = planService.process(data, operator, planName);
+		ArrayList<PlanChargeable> planCalls = summary.getPlanCalls();
+		PlanChargeable last = null;
+		for (int i = planCalls.size() - 1; i >= 0; i--) {
+			last = summary.getPlanCalls().get(i);
+			if (last.getChargeable() != null && last.getChargeable().getChargeableType() == Chargeable.CHARGEABLE_TYPE_CALL) {
+				break;
+			}
+		}
+		String lastCallPrice = Formatter.formatDecimal(last.getPrice()) + " " + last.getCurrency();
+		String totalPrice = Formatter.formatDecimal(summary.getTotalPrice()) + " " + last.getCurrency();
+		String text = lastCallPrice + " / " + totalPrice;
+		Notification notification = new Notification(R.drawable.app_icon, text, System.currentTimeMillis());
+		
+		Intent notificationIntent = new Intent(context, MainActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(context, context.getString(R.string.app_name), text, contentIntent);
+		notificationManager.notify(NOTIFICATION_ID, notification);
+	}
+	
 }
