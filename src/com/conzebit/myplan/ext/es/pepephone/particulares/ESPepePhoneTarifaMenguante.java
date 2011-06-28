98b94cf88f672ba71fb27b823757cad95d383a21
José Miguel Sanz <jmsanzg@gmail.com>
2011-06-28 17:33:47 +0200

    Issue #22 - Nueva Tarifa Menguante de PepePhone

---
+++ src/com/conzebit/myplan/ext/es/pepephone/particulares/ESPepePhoneTarifaMenguante.java
@@ -0,0 +1,79 @@
+/*
+ * This file is part of myPlan.
+ *
+ * Plan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Plan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with myPlan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.conzebit.myplan.ext.es.pepephone.particulares;
+
+import java.util.Map;
+
+import android.util.Log;
+
+import com.conzebit.myplan.core.call.Call;
+import com.conzebit.myplan.core.sms.Sms;
+import com.conzebit.myplan.ext.es.pepephone.ESPepePhone;
+
+
+/**
+ * PepePhone Tarifa Menguante
+ * 
+ * @author sanz
+ */
+public class ESPepePhoneTarifaMenguante extends ESPepePhone {
+    
+	private double monthFee = 6.9;
+	private double initialPrice = 0.15;
+	private double smsPrice = 0.09;
+	private double []pricePerSecond = {0.035/60, 0.03/60, 0.025/60, 0.02/60};
+	
+	@Override
+    public Double getMonthFee() {
+	    return monthFee;
+    }
+
+	public String getPlanName() {
+		return "Tarifa Menguante";
+	}
+	
+	public String getPlanURL() {
+		return "http://www.pepephone.com/promo/la_increible_tarifa_menguante/";
+	}
+	
+	public Double processCall(Call call, Map<String, Object> accumulatedData) {
+		if (call.getType() != Call.CALL_TYPE_SENT) {
+			return null;
+		}
+		
+		Long accumulatedSeconds = (Long) accumulatedData.get("TOTALMIN");
+		if (accumulatedSeconds == null) {
+			accumulatedSeconds = 0l;
+		}
+		
+		long step = (long) accumulatedSeconds / 3600;
+		if (step >= pricePerSecond.length) {
+			step = pricePerSecond.length - 1;
+		}
+		
+		accumulatedSeconds = accumulatedSeconds + call.getDuration();
+		accumulatedData.put("TOTALMIN", accumulatedSeconds);
+		return initialPrice + (call.getDuration() * pricePerSecond[(int) step]);
+	}
+
+	public Double processSms(Sms sms, Map<String, Object> accumulatedData) {
+		if (sms.getType() != Sms.SMS_TYPE_SENT) {
+			return null;
+		}
+		return smsPrice;
+	}
+}
