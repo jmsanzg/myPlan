522615bda43f1c47dd885fbac6db93cf897a53cc
José Miguel Sanz <jmsanzg@sanz-aspire.(none)>
2012-02-17 16:46:30 +0100

    #66 - Añadida Jazztel Habla Navega 5

---
+++ src/com/conzebit/myplan/ext/es/jazztel/particulares/ESJazztelHablaNavega5.java
@@ -0,0 +1,91 @@
+/*
+ * This file is part of myPlan.
+ *
+ * Plan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Plan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with myPlan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.conzebit.myplan.ext.es.jazztel.particulares;
+
+import java.util.ArrayList;
+
+import com.conzebit.myplan.core.Chargeable;
+import com.conzebit.myplan.core.call.Call;
+import com.conzebit.myplan.core.message.ChargeableMessage;
+import com.conzebit.myplan.core.msisdn.MsisdnType;
+import com.conzebit.myplan.core.plan.PlanChargeable;
+import com.conzebit.myplan.core.plan.PlanChargeable.Type;
+import com.conzebit.myplan.core.plan.PlanSummary;
+import com.conzebit.myplan.core.sms.Sms;
+import com.conzebit.myplan.ext.es.jazztel.ESJazztel;
+
+
+/**
+ * Jazztel Habla Navega 5.
+ * 
+ * @author sanz
+ */
+public class ESJazztelHablaNavega5 extends ESJazztel {
+    
+	private double monthFee = 11.95;
+	private double minimumMonthFee = 5;
+	private double initialPrice = 0.15;
+	private double pricePerSecond = 0.05 / 60;
+	private double smsPrice = 0.10;
+	// 500MB internet
+    
+	public String getPlanName() {
+		return "Habla Navega 5";
+	}
+	
+	public String getPlanURL() {
+		return "http://hogar.movil.jazztel.com/telefonia-movil/tarifa-llama-y-navega-jazzmovil-5";
+	}
+	
+	public PlanSummary process(ArrayList<Chargeable> data) {
+		PlanSummary ret = new PlanSummary(this);
+		ret.addPlanCall(new PlanChargeable(new ChargeableMessage(ChargeableMessage.MESSAGE_MONTH_FEE), monthFee, this.getCurrency(), Type.MONTH_FEE));
+		double globalPrice = 0;
+		for (Chargeable chargeable : data) {
+			if (chargeable.getChargeableType() == Chargeable.CHARGEABLE_TYPE_CALL) {
+				Call call = (Call) chargeable;
+
+				if (call.getType() != Call.CALL_TYPE_SENT) {
+					continue;
+				}
+	
+				double callPrice = 0;
+				Type type = null;
+				if (call.getContact().getMsisdnType() == MsisdnType.ES_SPECIAL_ZER0) {
+					callPrice = 0;
+					type = Type.ZERO;
+				} else {
+					callPrice = initialPrice + (call.getDuration() * pricePerSecond);
+					type = Type.INSIDE_PLAN;
+				}
+				globalPrice += callPrice;
+				ret.addPlanCall(new PlanChargeable(call, callPrice, this.getCurrency(), type));
+			} else if (chargeable.getChargeableType() == Chargeable.CHARGEABLE_TYPE_SMS) {
+				Sms sms = (Sms) chargeable;
+				if (sms.getType() == Sms.SMS_TYPE_RECEIVED) {
+					continue;
+				}
+				globalPrice += smsPrice;
+				ret.addPlanCall(new PlanChargeable(chargeable, smsPrice, this.getCurrency(), Type.INSIDE_PLAN));
+			}
+		}
+		if (globalPrice < minimumMonthFee) {
+			ret.addPlanCall(new PlanChargeable(new ChargeableMessage(ChargeableMessage.MESSAGE_MINIMUM_MONTH_FEE), minimumMonthFee - globalPrice, this.getCurrency(), Type.MINIMUM_MONTH_FEE));
+		}
+		return ret;
+	}
+}
