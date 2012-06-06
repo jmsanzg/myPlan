310b869106f0de039a5378553c2f97369f44c757
Adrian Almenar <aalmenar@gmail.com>
2012-06-06 11:21:24 +0200

    Planes adicionales de Happymovil

---
+++ src/com/conzebit/myplan/ext/es/happymovil/particulares/ESHappymovilSmartplan12.java
@@ -0,0 +1,96 @@
+/*
+ * This file is part of myPlan.
+ *
+ * Plan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * Plan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with myPlan.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.conzebit.myplan.ext.es.happymovil.particulares;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.conzebit.myplan.core.Chargeable;
+import com.conzebit.myplan.core.call.Call;
+import com.conzebit.myplan.core.message.ChargeableMessage;
+import com.conzebit.myplan.core.msisdn.MsisdnType;
+import com.conzebit.myplan.core.plan.PlanChargeable;
+import com.conzebit.myplan.core.plan.PlanSummary;
+import com.conzebit.myplan.core.sms.Sms;
+import com.conzebit.myplan.ext.es.happymovil.ESHappymovil;
+
+
+/**
+ * Smartplan 12
+ * 
+ * @author aalmenar
+ */
+public class ESHappymovilSmartplan12 extends ESHappymovil {
+    
+	private double monthFee = 12;
+	private double initialPrice = 0.15;
+	private double pricePerSecond = 0.08 / 60;
+	private double smsPrice = 0.09;
+	private int maxSecondsMonth = 100 * 60;
+    
+	public String getPlanName() {
+		return "Smartplan 12";
+	}
+	
+	public String getPlanURL() {
+		return "https://www.happymovil.es/contrato/habla-y-navega?id=20";
+	}
+	
+	public PlanSummary process(ArrayList<Chargeable> data) {
+		PlanSummary ret = new PlanSummary(this);
+		ret.addPlanCall(new PlanChargeable(new ChargeableMessage(ChargeableMessage.MESSAGE_MONTH_FEE), monthFee, this.getCurrency()));
+
+		Set<String> recipients = new HashSet<String>(); 
+		
+		int secondsTotal = 0;
+		for (Chargeable chargeable : data) {
+			if (chargeable.getChargeableType() == Chargeable.CHARGEABLE_TYPE_CALL) {
+				Call call = (Call) chargeable;
+			
+				if (call.getType() != Call.CALL_TYPE_SENT) {
+					continue;
+				}
+				
+				double callPrice = 0;
+				
+				if (call.getContact().getMsisdnType() == MsisdnType.ES_SPECIAL_ZER0) {
+					callPrice = 0;
+				} else {
+					recipients.add(call.getContact().getMsisdn());
+					int hourOfDay = call.getDate().get(Calendar.HOUR_OF_DAY);
+					int dayOfWeek = call.getDate().get(Calendar.DAY_OF_WEEK);
+					secondsTotal += call.getDuration();
+					boolean insidePlan = (hourOfDay < 8 || hourOfDay >= 18 || dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY) && secondsTotal <= maxSecondsMonth; 
+					if (!insidePlan) {
+						long duration = (secondsTotal > maxSecondsMonth) && (secondsTotal - call.getDuration() <= maxSecondsMonth)? secondsTotal - maxSecondsMonth : call.getDuration();  
+						callPrice += initialPrice + (duration * pricePerSecond);
+					}
+				}
+				ret.addPlanCall(new PlanChargeable(call, callPrice, this.getCurrency()));
+			} else if (chargeable.getChargeableType() == Chargeable.CHARGEABLE_TYPE_SMS) {
+				Sms sms = (Sms) chargeable;
+				if (sms.getType() == Sms.SMS_TYPE_RECEIVED) {
+					continue;
+				}
+				ret.addPlanCall(new PlanChargeable(chargeable, smsPrice, this.getCurrency()));
+			}
+		}
+		return ret;
+	}
+}
